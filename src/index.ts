492bbf43c1f5ad6034ee20d67d7365030fd38a43
khine-dev <khine.dev.9417@gmail.com>
2025-01-10 06:42:38 +0700

    microservice setup

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ app.get('/', (req : Request, res : Response) => {
   res.send('hello world from burmese chit chat AUTHENTICATION service');
 });
 
+app.get('/login', (req : Request, res : Response) => {
+  res.send('login page');
+});
+
 
 app.listen(PORT, () => {
     console.log('burmese chit chat authentication server is running on port ' + PORT);
